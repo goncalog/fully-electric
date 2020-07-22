32422d8c02e9c9fc83f1700a6baf00bc465b3f76
goncalog <goncalogordo@gmail.com>
2020-07-22 15:56:52 +0100

    Add EV model test

---
+++ fully-electric-backend/tests/models.test.js
@@ -0,0 +1,22 @@
+const EV = require('../models/ev');
+
+const ev = new EV({
+    make: "Tesla",
+    model: "S 70",
+    year: 2018,
+    price: 50000,
+    mileage: 18000,
+    location: "London, UK",
+    image_url: "https://placeholder.com/image111",
+    seller: "Miss Tesla",
+    list_date: "22/07/2020",
+    equipment_and_options: "placeholder",
+    exterior: "placeholder",
+    interior: "placeholder",
+    vehicle_identification_number: "1M8GDM9AXKP042788",
+    full_vehicle_inspection: true,
+});
+
+test('EV model exists', () => {
+    expect(ev).toBeInstanceOf(EV);
+});

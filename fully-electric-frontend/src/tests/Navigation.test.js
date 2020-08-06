c0c505b9d9ad1cc600e8d351269ddf6a68af00c5
goncalog <goncalogordo@gmail.com>
2020-08-06 15:14:10 +0100

    Test whether Navigation React component's Links point to the right path/href

--- fully-electric-frontend/src/tests/Navigation.test.js
+++ fully-electric-frontend/src/tests/Navigation.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -11,20 +11,30 @@ it('renders one child', () => {
     expect(React.Children.count(wrapper.children())).toEqual(1);
 });
 
-it('renders Home link', () => {
+it('renders Title link correctly', () => {
+    const { getByText } = render(<Router><Navigation /></Router>);
+    const titleLink = getByText(/Fully Electric/);
+    expect(titleLink).toBeInTheDocument();
+    expect(titleLink.getAttribute('href')).toBe('/');
+});
+
+it('renders Home link correctly', () => {
     const { getByText } = render(<Router><Navigation /></Router>);
     const homeLink = getByText(/Home/);
     expect(homeLink).toBeInTheDocument();
+    expect(homeLink.getAttribute('href')).toBe('/');
 });
 
-it('renders EVs link', () => {
+it('renders EVs link correctly', () => {
     const { getByText } = render(<Router><Navigation /></Router>);
     const evsLink = getByText(/EVs/);
     expect(evsLink).toBeInTheDocument();
+    expect(evsLink.getAttribute('href')).toBe('/content/evs');
 });
 
-it('renders Contact link', () => {
+it('renders Contact link correctly', () => {
     const { getByText } = render(<Router><Navigation /></Router>);
     const contactLink = getByText(/Contact/);
     expect(contactLink).toBeInTheDocument();
+    expect(contactLink.getAttribute('href')).toBe('/contact');
 });

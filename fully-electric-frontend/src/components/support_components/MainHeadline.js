9cb4cc65feb88f20c69d558514bdbe350fb6e621
goncalog <goncalogordo@gmail.com>
2020-08-14 17:13:37 +0100

    Add MainHeadline React component

---
+++ fully-electric-frontend/src/components/support_components/MainHeadline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MainHeadline extends React.Component {
+    render() {
+        return (
+            <h1 className="main-headline">Find your dream EV</h1>
+        );
+    }
+}
+
+export default MainHeadline;

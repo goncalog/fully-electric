187fb4d6d25e7c9324b12e0befd8649364cb6052
goncalog <goncalogordo@gmail.com>
2020-08-14 17:29:32 +0100

    Add SecondaryHeadline React component

---
+++ fully-electric-frontend/src/components/support_components/SecondaryHeadline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SecondaryHeadline extends React.Component {
+    render() {
+        return (
+            <h3 className="secondary-headline">Test drive it for FREE for one week before purchasing</h3>
+        );
+    }
+}
+
+export default SecondaryHeadline;

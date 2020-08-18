2b1dd5a7d546a36e611cd18155caabaf57f69626
goncalog <goncalogordo@gmail.com>
2020-08-18 17:00:52 +0100

    Update MainHeadline to use passed properties

--- fully-electric-frontend/src/components/support_components/MainHeadline.js
+++ fully-electric-frontend/src/components/support_components/MainHeadline.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class MainHeadline extends React.Component {
     render() {
+        const mainHeadlineText = this.props.mainHeadline;
+
         return (
-            <h1 className="main-headline">Find your dream EV</h1>
+            <h1 className="main-headline">{mainHeadlineText}</h1>
         );
     }
 }

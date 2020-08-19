512f6ea23777cb303ddf23528ec6ce4fad48ec14
goncalog <goncalogordo@gmail.com>
2020-08-19 15:02:55 +0100

    Add HeadlineContainer component to Home React component

--- fully-electric-frontend/src/components/Home.js
+++ fully-electric-frontend/src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import HeadlineContainer from './support_components/HeadlineContainer';
+import background from '../media/headline-background.jpg';
 
-function Home() {
-    return (
-        <h1>Home</h1>
-    );
+export default class Home extends React.Component {
+    render() {
+        const props = {
+            backgroundImagePath: background,
+            mainHeadline: 'Find your dream EV',
+            secondaryHeadline: 'Test drive it for FREE for one week before purchasing',
+            callToActionText: 'Let\'s GO!',
+        }
+
+        return (
+            <div className="home">
+                <HeadlineContainer {...props}/>
+            </div>
+        );
+    }
 }
-
-export default Home;

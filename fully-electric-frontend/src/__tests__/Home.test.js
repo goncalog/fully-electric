20e8656a9a87142a23035b1a0e5a2807c75b5c56
goncalog <goncalogordo@gmail.com>
2020-08-19 17:23:09 +0100

    Add tests for BenefitsContainer React component to Home React component

--- fully-electric-frontend/src/__tests__/Home.test.js
+++ fully-electric-frontend/src/__tests__/Home.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from '../components/Home';
 import HeadlineContainer from '../components/support_components/HeadlineContainer';
+import BenefitsContainer from '../components/support_components/BenefitsContainer';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -22,20 +23,30 @@ describe('Home', () => {
         shallowHome = undefined;
     });
 
-    test('has one child', () => {
-        expect(home().children().length).toEqual(1);
+    test('has two children', () => {
+        expect(home().children().length).toEqual(2);
     });
 
     test('has one HeadlineContainer', () => {
         expect(home().find(HeadlineContainer).length).toEqual(1);
     });
 
-    test('passes properties to child', () => {
+    test('renders HeadlineContainer with passed properties', () => {
         const shallowWrapper = home().find(HeadlineContainer);
         expect(shallowWrapper.prop('backgroundImagePath')).toBe('headline-background.jpg');
         expect(shallowWrapper.prop('mainHeadline')).toBe('Find your dream EV');
         expect(shallowWrapper.prop('secondaryHeadline'))
                 .toBe('Test drive it for FREE for one week before purchasing');
+        expect(shallowWrapper.prop('callToActionText')).toBe('Let\'s GO!');
+    });
+
+    test('has one BenefitsContainer', () => {
+        expect(home().find(BenefitsContainer).length).toEqual(1);
+    });
+
+    test('renders BenefitsContainer with passed properties', () => {
+        const shallowWrapper = home().find(BenefitsContainer);
+        expect(shallowWrapper.prop('benefits').length).toEqual(3);
         expect(shallowWrapper.prop('callToActionText')).toBe('Let\'s GO!');
     });
 });

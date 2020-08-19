c6797afa690e33b382eb44c66db2745919ef0831
goncalog <goncalogordo@gmail.com>
2020-08-19 17:32:42 +0100

    Add BenefitsContainer React component to Home React component

--- fully-electric-frontend/src/components/Home.js
+++ fully-electric-frontend/src/components/Home.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import HeadlineContainer from './support_components/HeadlineContainer';
+import BenefitsContainer from './support_components/BenefitsContainer';
 import background from '../media/headline-background.jpg';
 import '../css/Home.css';
 
 export default class Home extends React.Component {
     render() {
-        const props = {
+        const headlineProps = {
             backgroundImagePath: background,
             mainHeadline: 'Find your dream EV',
             secondaryHeadline: 'Test drive it for FREE for one week before purchasing',
             callToActionText: 'Let\'s GO!',
         }
 
+        const benefitsProps = {
+            benefits: [
+                { 
+                    title: 'Best Price',
+                    text: 'Find the best EV deals available online',
+                },
+                { 
+                    title: 'Most Convenient',
+                    text: 'We bring it to your home at the time of your preference',
+                },
+                { 
+                    title: 'No Risk',
+                    text: 'Test drive your dream EV for one week before deciding if it\'s right for you - completely FREE!',
+                },
+            ],
+            callToActionText: 'Let\'s GO!',
+        }
+
         return (
             <div className="home">
-                <HeadlineContainer {...props}/>
+                <HeadlineContainer {...headlineProps}/>
+                <BenefitsContainer {...benefitsProps} />
             </div>
         );
     }

466f34a73b04503f24c6b1ec778a9e27729bd4a4
goncalog <goncalogordo@gmail.com>
2020-08-21 14:27:02 +0100

    Add tests for SellerContainer React component

---
+++ fully-electric-frontend/src/__tests__/support_components/SellerContainer.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Headline from '../../components/support_components/Headline';
+import CallToActionButton from '../../components/support_components/CallToActionButton';
+import SellerContainer from '../../components/support_components/SellerContainer';
+import { configure, shallow, ShallowWrapper } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe('SellerContainer', () => {
+    let props;
+    let shallowSellerContainer;
+    const sellerContainer = () => {
+        if (!shallowSellerContainer) {
+            shallowSellerContainer = shallow(<SellerContainer {...props} />);
+        }
+        return shallowSellerContainer;
+    }
+
+    // This reset the props and shallowSellerContainer variables before every test. 
+    // Otherwise, state from one test would leak into another. 
+    // By setting shallowSellerContainer to undefined here, when the next test runs, 
+    // if it calls sellerContainer, a new SellerContainer will be created with the current props
+    beforeEach(() =>  {
+        props = {
+            mainHeadline: 'Text to test mainHeadline property',
+            secondaryHeadline: 'Text to test secondaryHeadline property',
+            callToActionText: 'Text to test callToActionText property',
+        }
+        shallowSellerContainer = undefined;
+    });
+
+    test('has 2 children', () => {
+        expect(sellerContainer().children().length).toEqual(2);
+    });
+
+    test('has one Headline component rendered with passed properties', () => {
+        const shallowWrapper = sellerContainer().find(Headline);
+        expect(shallowWrapper.length).toEqual(1);
+        expect(shallowWrapper.prop('mainHeadline')).toBe(props.mainHeadline);
+        expect(shallowWrapper.prop('secondaryHeadline')).toBe(props.secondaryHeadline);
+    });
+
+    test('has one CallToActionButton component rendered with passed property', () => {
+        const shallowWrapper = sellerContainer().find(CallToActionButton);
+        expect(shallowWrapper.length).toEqual(1);
+        expect(shallowWrapper.prop('callToActionText')).toBe(props.callToActionText);
+    });
+});

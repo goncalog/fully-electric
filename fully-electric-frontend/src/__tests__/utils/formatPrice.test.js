81485c9ed6e6e1128a8de63ffc7ca629710517a4
goncalog <goncalogordo@gmail.com>
2020-08-21 17:35:20 +0100

    Update tests for formatPrice util function

--- fully-electric-frontend/src/__tests__/utils/formatPrice.test.js
+++ fully-electric-frontend/src/__tests__/utils/formatPrice.test.js
@@ -9,5 +9,7 @@ describe('formatPrice', () => {
         expect(formatPrice('uk', '1000')).toBe('£1,000');
         expect(formatPrice('uk', '10000')).toBe('£10,000');
         expect(formatPrice('uk', '50000')).toBe('£50,000');
+        expect(formatPrice('uk', '100000')).toBe('£100,000');
+        expect(formatPrice('uk', '500000')).toBe('£500,000');
     });
 });

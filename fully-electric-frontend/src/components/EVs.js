1c3a13b37cfa49a05246bac65cc49820ae04eaef
goncalog <goncalogordo@gmail.com>
2020-08-22 15:47:00 +0100

    Add EVsContainer React component to EVs React component

--- fully-electric-frontend/src/components/EVs.js
+++ fully-electric-frontend/src/components/EVs.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import EVsContainer from './support_components/EVsContainer';
 
-function EVs() {
-    return (
-        <h1>EVs</h1>
-    );
+export default class EVs extends React.Component {
+    render() {
+        const evs = [
+            {
+                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.PYF7tsBxVqwkqGyeOZdVXwHaE8%26pid%3DApi&f=1',
+                title: 'Tesla Model 3',
+                price: '35000',
+                evFeatures: [
+                    { 
+                        name: 'Year',
+                        value: '2019',
+                    },
+                    { 
+                        name: 'Mileage',
+                        value: '12550',
+                    },
+                    { 
+                        name: 'Range',
+                        value: '200 miles',
+                    },
+                ],    
+            },
+            {
+                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Qyz9YaDUbM866vK5U6RRVQHaE7%26pid%3DApi&f=1',
+                title: 'Nissan Leaf',
+                price: '12000',
+                evFeatures: [
+                    { 
+                        name: 'Year',
+                        value: '2017',
+                    },
+                    { 
+                        name: 'Mileage',
+                        value: '23550',
+                    },
+                    { 
+                        name: 'Range',
+                        value: '80 miles',
+                    },
+                ], 
+            },
+            {
+                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tIJz9tL27d_AaByqyWaHwAHaEK%26pid%3DApi&f=1',
+                title: 'Hyundai Ioniq',
+                price: '25000',
+                evFeatures: [
+                    { 
+                        name: 'Year',
+                        value: '2018',
+                    },
+                    { 
+                        name: 'Mileage',
+                        value: '52550',
+                    },
+                    { 
+                        name: 'Range',
+                        value: '100 miles',
+                    },
+                ], 
+            },
+            {
+                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.2y7YXs-cAIR_s9d-uw1VGwHaEn%26pid%3DApi&f=1',
+                title: 'Renault Zoe',
+                price: '22000',
+                evFeatures: [
+                    { 
+                        name: 'Year',
+                        value: '2015',
+                    },
+                    { 
+                        name: 'Mileage',
+                        value: '72550',
+                    },
+                    { 
+                        name: 'Range',
+                        value: '80 miles',
+                    },
+                ], 
+            },
+        ];
+
+        return (
+            <div className="evs">
+                <EVsContainer evs={evs} />
+            </div>
+        );
+    }
 }
-
-export default EVs;

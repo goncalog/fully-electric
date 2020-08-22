062cfdce990b153f25cb72cc290ad14f872848b9
goncalog <goncalogordo@gmail.com>
2020-08-22 17:02:23 +0100

    Add route for EV React component to AppRouter React component

--- fully-electric-frontend/src/components/AppRouter.js
+++ fully-electric-frontend/src/components/AppRouter.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import EVs from './EVs';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import EV from './EV';
 
 function AppRouter() {
     return (
@@ -12,6 +13,7 @@ function AppRouter() {
             <Switch>
                 <Route path='/' exact component={Home}></Route>
                 <Route path='/content/evs' exact component={EVs}></Route>
+                <Route path='/content/ev/:id' exact component={EV}></Route>
                 <Route path='/contact' exact component={Contact}></Route>
             </Switch>
         </Router>

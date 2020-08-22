b8ebbc88f4efe6fc37ecf54dc9e729b9b9d3560a
goncalog <goncalogordo@gmail.com>
2020-08-22 17:01:23 +0100

    Add Link React components to EVsContainer React component

--- fully-electric-frontend/src/components/support_components/EVsContainer.js
+++ fully-electric-frontend/src/components/support_components/EVsContainer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import EVIntroCard from './EVIntroCard';
+import { Link } from 'react-router-dom';
 
 export default function EVsContainer(props) {
     return (
         <div className="evs-container">
             {props.evs.map((ev, index) => {
                 return (
-                    <EVIntroCard 
-                        imagePath={ev.imagePath} 
-                        title={ev.title} 
-                        price={ev.price} 
-                        evFeatures={ev.evFeatures} 
-                        key={index} 
-                    />
+                    <Link to={`/content/ev/${index}`} key={index}>
+                        <EVIntroCard 
+                            imagePath={ev.imagePath} 
+                            title={ev.title} 
+                            price={ev.price} 
+                            evFeatures={ev.evFeatures} 
+                            key={index} 
+                        />
+                    </Link> 
                 );
             })}
         </div>

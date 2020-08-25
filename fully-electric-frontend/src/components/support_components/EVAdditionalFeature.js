25c348fb16e03fb1f34c6c1e52cff2589f3e2521
goncalog <goncalogordo@gmail.com>
2020-08-25 15:58:30 +0100

    Remove EVAdditionalFeature React component

--- fully-electric-frontend/src/components/support_components/EVAdditionalFeature.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function EVAdditionalFeature(props) {
-    return (
-        <p className="additional-feature">{`- ${props.name}: ${props.value}`}</p>
-    );
-}

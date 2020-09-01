9dcd145f85447a05a65d0d2048955d1fd3762deb
goncalog <goncalogordo@gmail.com>
2020-09-01 20:33:47 +0100

    Add performance properties to mongoose's Model model

--- fully-electric-backend/models/model.js
+++ fully-electric-backend/models/model.js
@@ -6,7 +6,12 @@ const ModelSchema = new Schema({
     make: { type: Schema.Types.ObjectId, ref: 'Make', required: true },
     name: { type: String, required: true },
     secondary_name: { type: String },
-    performance: { type: String, required: true },
+    performance: {
+        horsepower: { type: Number, required: true, min: 0 },
+        miles_per_kwh: { type: Number, min: 0 },
+        top_speed_mph: { type: Number, required: true, min: 0 },
+        zero_to_sixty_mph: { type: Number, required: true, min: 0 },
+    },
     charging: { type: String, required: true },
     original_msrp: { type: Number, required: true, min: 0 },
     rating: { type: Number, required: true, min: 0, max: 5 },

5481206d27d5eed38bb485e88ffded86a9d87cc3
goncalog <goncalogordo@gmail.com>
2020-09-02 15:41:24 +0100

    Update EV's image urls to be a String array

--- fully-electric-backend/models/ev.js
+++ fully-electric-backend/models/ev.js
@@ -11,7 +11,7 @@ const EVSchema = new Schema({
     price: { type: Number, required: true, min: 0 },
     mileage: { type: Number, required: true, min: 0 },
     location: { type: Schema.Types.ObjectId, ref: 'Location', required: true },
-    image_url: { type: String, required: true },
+    image_urls: { type: [String], required: true, default: undefined }, // by default this would be an empty array which would affect the way we then create new instances of this model (i.e. it would require using array.push(item))
     seller: { type: Schema.Types.ObjectId, ref: 'Seller', required: true },
     list_date: { type: Date, default: Date.now, required: true },
     equipment_and_options: { type: [String], required: true, default: undefined }, // by default this would be an empty array which would affect the way we then create new instances of this model (i.e. it would require using array.push(item))

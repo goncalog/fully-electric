ab26f60add34dded1f68897316439579eb84e94d
goncalog <goncalogordo@gmail.com>
2020-09-02 16:33:59 +0100

    Update populate database script to include new model and ev properties

--- fully-electric-backend/database/populatedb.js
+++ fully-electric-backend/database/populatedb.js
@@ -101,7 +101,7 @@ function sellerCreate(name, contact, rating, cb) {
     });
 }
 
-function evCreate(make, model, year, price, mileage, location, imageUrl, seller, listDate, 
+function evCreate(make, model, year, price, mileage, location, imageUrls, seller, listDate, 
             equipmentAndOptions, exterior, interior, vehicleIdentificationNumber, 
             fullVehicleInspection, cb) {
     evDetail = { 
@@ -111,7 +111,7 @@ function evCreate(make, model, year, price, mileage, location, imageUrl, seller,
         price: price,
         mileage: mileage,
         location: location,
-        image_url: imageUrl,
+        image_urls: imageUrls,
         seller: seller,
         list_date: listDate,
         equipment_and_options: equipmentAndOptions,
@@ -182,43 +182,82 @@ function createMakes(cb) {
 function createModels(cb) {
     async.series([
         function (callback) {
-            modelCreate(makes[0], 'Model S', false, 'High', 'Fast', 80000, 4.7, callback);
+            modelCreate(makes[0], 'Model S', false, 
+            { horsepower: 400, miles_per_kwh: 4, top_speed_mph: 150, zero_to_sixty_mph: 3.2 },
+            { range_miles: 300, battery_size_kwh: 75, charge_cost: 8, hours_to_charge: 7 },
+            80000, 4.7, callback);
         },
         function (callback) {
-            modelCreate(makes[0], 'Model 3', false, 'High', 'Fast', 50000, 4.6, callback);
+            modelCreate(makes[0], 'Model 3', false,
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            50000, 4.6, callback);
         },
         function (callback) {
-            modelCreate(makes[1], 'Leaf', false, 'Medium', 'Fast', 35000, 4.5, callback);
+            modelCreate(makes[1], 'Leaf', false, 
+            { horsepower: 200, miles_per_kwh: 3, top_speed_mph: 100, zero_to_sixty_mph: 8.2 },
+            { range_miles: 120, battery_size_kwh: 40, charge_cost: 5, hours_to_charge: 5 },
+            35000, 4.5, callback);
         },
         function (callback) {
-            modelCreate(makes[2], 'Zoe', false, 'Medium', 'Fast', 35000, 4.7, callback);
+            modelCreate(makes[2], 'Zoe', false, 
+            { horsepower: 200, miles_per_kwh: 3, top_speed_mph: 100, zero_to_sixty_mph: 8.2 },
+            { range_miles: 120, battery_size_kwh: 40, charge_cost: 5, hours_to_charge: 5 },
+            35000, 4.7, callback);
         },
         function (callback) {
-            modelCreate(makes[3], 'Kona', false, 'Medium', 'Fast', 45000, 4.5, callback);
+            modelCreate(makes[3], 'Kona', false, 
+            { horsepower: 200, miles_per_kwh: 3, top_speed_mph: 100, zero_to_sixty_mph: 8.2 },
+            { range_miles: 120, battery_size_kwh: 40, charge_cost: 5, hours_to_charge: 5 },
+            45000, 4.5, callback);
         },
         function (callback) {
-            modelCreate(makes[4], 'e-Niro', false, 'Medium', 'Fast', 40000, 4.5, callback);
+            modelCreate(makes[4], 'e-Niro', false, 
+            { horsepower: 200, miles_per_kwh: 3, top_speed_mph: 100, zero_to_sixty_mph: 8.2 },
+            { range_miles: 120, battery_size_kwh: 40, charge_cost: 5, hours_to_charge: 5 },
+            40000, 4.5, callback);
         },
         function (callback) {
-            modelCreate(makes[5], 'e-Golf', false, 'Medium', 'Fast', 50000, 4.7, callback);
+            modelCreate(makes[5], 'e-Golf', false, 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            50000, 4.7, callback);
         },
         function (callback) {
-            modelCreate(makes[6], 'i3', false, 'High', 'Fast', 60000, 4.6, callback);
+            modelCreate(makes[6], 'i3', false, 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            60000, 4.6, callback);
         },
         function (callback) {
-            modelCreate(makes[7], 'e-tron', '55', 'High', 'Fast', 50000, 4.5, callback);
+            modelCreate(makes[7], 'e-tron', '55', 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            50000, 4.5, callback);
         },
         function (callback) {
-            modelCreate(makes[8], 'EQC', false, 'High', 'Fast', 70000, 4.6, callback);
+            modelCreate(makes[8], 'EQC', false, 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            70000, 4.6, callback);
         },
         function (callback) {
-            modelCreate(makes[9], 'I-Pace', false, 'High', 'Fast', 70000, 4.6, callback);
+            modelCreate(makes[9], 'I-Pace', false, 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            70000, 4.6, callback);
         },
         function (callback) {
-            modelCreate(makes[10], '2', false, 'High', 'Fast', 60000, 4.5, callback);
+            modelCreate(makes[10], '2', false, 
+            { horsepower: 300, miles_per_kwh: 4, top_speed_mph: 130, zero_to_sixty_mph: 3.9 },
+            { range_miles: 240, battery_size_kwh: 60, charge_cost: 7, hours_to_charge: 6 },
+            60000, 4.5, callback);
         },
         function (callback) {
-            modelCreate(makes[11], 'e208', false, 'Medium', 'Fast', 35000, 4.5, callback);
+            modelCreate(makes[11], 'e208', false, 
+            { horsepower: 200, miles_per_kwh: 3, top_speed_mph: 100, zero_to_sixty_mph: 8.2 },
+            { range_miles: 120, battery_size_kwh: 40, charge_cost: 5, hours_to_charge: 5 },
+            35000, 4.5, callback);
         },
     ],
     // Optional callback
@@ -315,12 +354,14 @@ function createEvs(cb) {
                 43000,
                 24550,
                 locations[0],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-d1rVTq_5plECVY8Pm47gHaE8%26pid%3DApi&f=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-d1rVTq_5plECVY8Pm47gHaE8%26pid%3DApi&f=1', 
+                        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FL7pk4vKDg0A%2Fmaxresdefault.jpg&f=1&nofb=1',
+                        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.astonmartinwashingtondc.com%2Fimagetag%2F1544%2F7%2Fl%2FUsed-2016-Tesla-Model-S-P100D.jpg&f=1&nofb=1'],
                 sellers[0],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Black' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback
@@ -334,12 +375,12 @@ function createEvs(cb) {
                 24000,
                 34550,
                 locations[1],
-                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.hgmsites.net%2Fhug%2F2016-nissan-leaf_100527043_h.jpg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.hgmsites.net%2Fhug%2F2016-nissan-leaf_100527043_h.jpg&f=1&nofb=1'],
                 sellers[1],
                 Date.now,
-                'Standard',
-                'Standard',
-                'Standard',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Blue' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback
@@ -353,12 +394,12 @@ function createEvs(cb) {
                 17250,
                 54500,
                 locations[2],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.cdn.autocar.co.uk%2Fsites%2Fautocar.co.uk%2Ffiles%2Fstyles%2Fgallery_slide%2Fpublic%2Frenault-zoe.jpg%3Fitok%3DpnEKK1ba&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.cdn.autocar.co.uk%2Fsites%2Fautocar.co.uk%2Ffiles%2Fstyles%2Fgallery_slide%2Fpublic%2Frenault-zoe.jpg%3Fitok%3DpnEKK1ba&f=1&nofb=1'],
                 sellers[2],
                 Date.now,
-                'Standard',
-                'Standard',
-                'Standard',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Red' },
+                { seating: 5, colour: 'Beige' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback
@@ -372,12 +413,12 @@ function createEvs(cb) {
                 37500,
                 34500,
                 locations[3],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Ff%2Ff9%2F2018_Hyundai_Kona_SE_1.0.jpg%2F1200px-2018_Hyundai_Kona_SE_1.0.jpg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Ff%2Ff9%2F2018_Hyundai_Kona_SE_1.0.jpg%2F1200px-2018_Hyundai_Kona_SE_1.0.jpg&f=1&nofb=1'],
                 sellers[3],
                 Date.now,
-                'Premium',
-                'Standard',
-                'Standard',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Black' },
+                { seating: 5, colour: 'Grey' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback
@@ -391,12 +432,12 @@ function createEvs(cb) {
                 23500,
                 14500,
                 locations[4],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.drivingelectric.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Farticle_image_desktop%2Fpublic%2F2018-11%2F2kiae-niro.jpg%3Fh%3Dc3635fa2%26itok%3DhuFgMWcL&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.drivingelectric.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Farticle_image_desktop%2Fpublic%2F2018-11%2F2kiae-niro.jpg%3Fh%3Dc3635fa2%26itok%3DhuFgMWcL&f=1&nofb=1'],
                 sellers[4],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Standard',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'White' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -410,12 +451,12 @@ function createEvs(cb) {
                 13500,
                 34500,
                 locations[5],
-                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.telegraph.co.uk%2Fcars%2Fimages%2F2017%2F06%2F20%2FTELEMMGLPICT000130092501-xlarge_trans_NvBQzQNjv4BqJQuPXpcEMOatKkwW02PS65oSssFO5HHodOf-e6p-uYU.jpeg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.telegraph.co.uk%2Fcars%2Fimages%2F2017%2F06%2F20%2FTELEMMGLPICT000130092501-xlarge_trans_NvBQzQNjv4BqJQuPXpcEMOatKkwW02PS65oSssFO5HHodOf-e6p-uYU.jpeg&f=1&nofb=1'],
                 sellers[5],
                 Date.now,
-                'Premium',
-                'Standard',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'White' },
+                { seating: 5, colour: 'Grey' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback
@@ -429,12 +470,12 @@ function createEvs(cb) {
                 30500,
                 66000,
                 locations[6],
-                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.cdn.autocar.co.uk%2Fsites%2Fautocar.co.uk%2Ffiles%2Fstyles%2Fgallery_slide%2Fpublic%2FBMWi3-Stan_30125.jpg%3Fitok%3DqRtk_UkH&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.cdn.autocar.co.uk%2Fsites%2Fautocar.co.uk%2Ffiles%2Fstyles%2Fgallery_slide%2Fpublic%2FBMWi3-Stan_30125.jpg%3Fitok%3DqRtk_UkH&f=1&nofb=1'],
                 sellers[6],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Grey' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -448,12 +489,12 @@ function createEvs(cb) {
                 38500,
                 12400,
                 locations[7],
-                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hdcarwallpapers.com%2Fwalls%2Faudi_e_tron_55_quattro_2019_4k_5-HD.jpg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hdcarwallpapers.com%2Fwalls%2Faudi_e_tron_55_quattro_2019_4k_5-HD.jpg&f=1&nofb=1'],
                 sellers[7],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Black' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -467,12 +508,12 @@ function createEvs(cb) {
                 58000,
                 22700,
                 locations[8],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.autoexpress.co.uk%2Fimage%2Fprivate%2Fs--Wzb4sPbF--%2Fv1565018660%2Fautoexpress%2F2019%2F08%2F01_2.jpg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.autoexpress.co.uk%2Fimage%2Fprivate%2Fs--Wzb4sPbF--%2Fv1565018660%2Fautoexpress%2F2019%2F08%2F01_2.jpg&f=1&nofb=1'],
                 sellers[8],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'White' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -486,12 +527,12 @@ function createEvs(cb) {
                 55000,
                 32700,
                 locations[9],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F3%2F37%2F2018_Jaguar_I-Pace_EV400_AWD_Front.jpg%2F1200px-2018_Jaguar_I-Pace_EV400_AWD_Front.jpg&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F3%2F37%2F2018_Jaguar_I-Pace_EV400_AWD_Front.jpg%2F1200px-2018_Jaguar_I-Pace_EV400_AWD_Front.jpg&f=1&nofb=1'],
                 sellers[9],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'White' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -505,12 +546,12 @@ function createEvs(cb) {
                 52000,
                 8700,
                 locations[10],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F9to5google.com%2Fwp-content%2Fuploads%2Fsites%2F4%2F2019%2F02%2Fpolestar_2_1.jpg%3Fquality%3D82%26strip%3Dall&f=1&nofb=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F9to5google.com%2Fwp-content%2Fuploads%2Fsites%2F4%2F2019%2F02%2Fpolestar_2_1.jpg%3Fquality%3D82%26strip%3Dall&f=1&nofb=1'],
                 sellers[10],
                 Date.now,
-                'Premium',
-                'Premium',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Dark Grey' },
+                { seating: 5, colour: 'Black' },
                 'XXXXXXXXXXXXXXXXX',
                 false,
                 callback
@@ -524,12 +565,12 @@ function createEvs(cb) {
                 28800,
                 48705,
                 locations[11],
-                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.iJnLtTabyraAS5eG4XgT2AHaFj%26pid%3DApi&f=1',
+                ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.iJnLtTabyraAS5eG4XgT2AHaFj%26pid%3DApi&f=1'],
                 sellers[11],
                 Date.now,
-                'Standard',
-                'Standard',
-                'Premium',
+                ['Air conditioning, Brake assistance, Traction control', 'Speed control'],
+                { colour: 'Blue' },
+                { seating: 5, colour: 'Dark Grey' },
                 'XXXXXXXXXXXXXXXXX',
                 true,
                 callback

be0657899f53b818b16e0f25d94128cda30bb6de
goncalog <goncalogordo@gmail.com>
2020-09-02 20:51:01 +0100

    Fix path to .env file

--- fully-electric-backend/database/mongoConfig.js
+++ fully-electric-backend/database/mongoConfig.js
@@ -1,7 +1,7 @@
 // This is in its own file so the test files can use a different config file 
 // that sets up mongodb-memory-server
 
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/../.env' });
 
 const mongoose = require('mongoose');
 // const Schema = mongoose.Schema;

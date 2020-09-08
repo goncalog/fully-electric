376d493e6199877aa63849597066b29bff87181a
goncalog <goncalogordo@gmail.com>
2020-09-08 15:56:54 +0100

    Add API call to EVs React component in order to get the list of all evs in the database

--- fully-electric-frontend/src/components/EVs.js
+++ fully-electric-frontend/src/components/EVs.js
@@ -4,98 +4,43 @@ import EVsContainer from './support_components/EVsContainer';
 export default class EVs extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { message: 'Initial message' };
+        this.state = { evs: [] };
     }
 
     componentDidMount() {
         fetch(`${process.env.REACT_APP_SERVER_URL}/content/evs`)
             .then(res => res.json())
-            .then((res) => { this.setState({ message: res.title }) })
+            .then((res) => { this.setState({ evs: res.evs }) })
     }
 
     render() {
-        const evs = [
-            {
-                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.PYF7tsBxVqwkqGyeOZdVXwHaE8%26pid%3DApi&f=1',
-                title: 'Tesla Model 3',
-                price: '35000',
+        const evs = this.state.evs.map((item) => {
+            let ev = {
+                imageUrls: item.image_urls,
+                title: (item.model.secondary_name) ? 
+                        `${item.make.name} ${item.model.name} ${item.model.secondary_name}` : 
+                        `${item.make.name} ${item.model.name}`,
+                price: item.price.toString(),
                 evFeatures: [
                     { 
                         name: 'Year',
-                        value: '2019',
+                        value: item.year.toString(),
                     },
                     { 
                         name: 'Mileage',
-                        value: '12550',
+                        value: item.mileage.toString(),
                     },
                     { 
                         name: 'Range',
-                        value: '200 miles',
+                        value: item.model.charging.range_miles.toString(),
                     },
-                ],    
-            },
-            {
-                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Qyz9YaDUbM866vK5U6RRVQHaE7%26pid%3DApi&f=1',
-                title: 'Nissan Leaf',
-                price: '12000',
-                evFeatures: [
-                    { 
-                        name: 'Year',
-                        value: '2017',
-                    },
-                    { 
-                        name: 'Mileage',
-                        value: '23550',
-                    },
-                    { 
-                        name: 'Range',
-                        value: '80 miles',
-                    },
-                ], 
-            },
-            {
-                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tIJz9tL27d_AaByqyWaHwAHaEK%26pid%3DApi&f=1',
-                title: 'Hyundai Ioniq',
-                price: '25000',
-                evFeatures: [
-                    { 
-                        name: 'Year',
-                        value: '2018',
-                    },
-                    { 
-                        name: 'Mileage',
-                        value: '52550',
-                    },
-                    { 
-                        name: 'Range',
-                        value: '100 miles',
-                    },
-                ], 
-            },
-            {
-                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.2y7YXs-cAIR_s9d-uw1VGwHaEn%26pid%3DApi&f=1',
-                title: 'Renault Zoe',
-                price: '22000',
-                evFeatures: [
-                    { 
-                        name: 'Year',
-                        value: '2015',
-                    },
-                    { 
-                        name: 'Mileage',
-                        value: '72550',
-                    },
-                    { 
-                        name: 'Range',
-                        value: '80 miles',
-                    },
-                ], 
-            },
-        ];
+                ],                
+            };
+            return ev;
+        });
 
         return (
             <div className="evs">
-                <h1>{this.state.message}</h1>
                 <EVsContainer evs={evs} />
             </div>
         );

3d5091f7f63b11394a8b0ce940ba9b5f35eef382
goncalog <goncalogordo@gmail.com>
2020-09-09 16:00:43 +0100

    Fetch API data from EV React component

--- fully-electric-frontend/src/components/EV.js
+++ fully-electric-frontend/src/components/EV.js
@@ -4,105 +4,127 @@ import EVPrice from './support_components/EVPrice';
 import SellerContact from './support_components/SellerContact';
 import EVDetail from './support_components/EVDetail';
 import formatRating from '../utils/formatRating';
+import getFullEvTitle from '../utils/getFullEvTitle';
 
 export default class EV extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { ev: {} };
+    }
+
+    componentDidMount() {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/content/ev/${this.props.match.params.id}`)
+            .then((res) => res.json())
+            .then((res) => { this.setState({ ev: res.ev }) })
+    }
+
     render() {
-        const ev = {
-            title: 'Tesla Model 3',
-            price: '35000',
-            seller: {
-                name: 'Miss Tesla',
-                rating: '4.67',
-                callToActionText: 'Contact Seller',
-            },
-            detail: {
-                imagePath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.PYF7tsBxVqwkqGyeOZdVXwHaE8%26pid%3DApi&f=1',
-                evFeatures: [
-                    { 
-                        name: 'Year',
-                        value: '2019',
-                    },
-                    { 
-                        name: 'Mileage',
-                        value: '12550',
-                    },
-                    { 
-                        name: 'Range',
-                        value: '200 miles',
-                    },
-                    { 
-                        name: 'Location',
-                        value: 'London',
-                    },
-                    { 
-                        name: 'Rating',
-                        value: formatRating('4.78'),
-                    },
-                ],
-                sections: [
-                    {
-                        name: 'Equipment and options',
-                        expandButtonText: '+',
-                        evFeatures: [
-                            { 
-                                name: 'Equipment',
-                                value: 'Premium',
-                            },
-                            { 
-                                name: 'Options',
-                                value: 'Standard',
-                            },
-                        ],    
-                    },
-                    {
-                        name: 'Exterior',
-                        expandButtonText: '+',
-                        evFeatures: [
-                            { 
-                                name: 'Colour',
-                                value: 'Red',
-                            },
-                            { 
-                                name: 'Doors',
-                                value: '5',
-                            },
-                        ],    
-                    },
-                    {
-                        name: 'Interior',
-                        expandButtonText: '+',
-                        evFeatures: [
-                            { 
-                                name: 'Seats',
-                                value: 'Leather',
-                            },
-                            { 
-                                name: 'Screen',
-                                value: 'Touch',
-                            },
-                        ],    
-                    },
-                    {
-                        name: 'Performance',
-                        expandButtonText: '+',
-                        evFeatures: [
-                            { 
-                                name: '0-60mph',
-                                value: '4.0sec',
-                            },
-                            { 
-                                name: 'Top speed',
-                                value: '162mph',
-                            },
-                        ],    
-                    },
-                ],
-            },     
+        console.log(this.state.ev);
+
+        let ev = {
+            title: '', 
+            price: '',
+            seller: { name: '', rating: '', callToActionText: '', },
+            detail: { imageUrls: [], evFeatures: [], sections: [], },     
+        }
+
+        if (Object.keys(this.state.ev).length > 0) {
+            ev = {
+                title: getFullEvTitle(this.state.ev),
+                price: this.state.ev.price.toString(),
+                seller: {
+                    name: this.state.ev.seller.name,
+                    rating: this.state.ev.seller.rating,
+                    callToActionText: 'Contact Seller',
+                },
+                detail: {
+                    imageUrls: this.state.ev.image_urls,
+                    evFeatures: [
+                        { 
+                            name: 'Year',
+                            value: this.state.ev.year,
+                        },
+                        { 
+                            name: 'Mileage',
+                            value: this.state.ev.mileage,
+                        },
+                        { 
+                            name: 'Range',
+                            value: this.state.ev.model.charging.range_miles,
+                        },
+                        { 
+                            name: 'Location',
+                            value: this.state.ev.location.city,
+                        },
+                        { 
+                            name: 'Rating',
+                            value: formatRating(this.state.ev.model.rating),
+                        },
+                    ],
+                    sections: [
+                        {
+                            name: 'Equipment and options',
+                            expandButtonText: '+',
+                            evFeatures: [
+                                { 
+                                    name: 'Equipment',
+                                    value: 'Premium',
+                                },
+                                { 
+                                    name: 'Options',
+                                    value: 'Standard',
+                                },
+                            ],    
+                        },
+                        {
+                            name: 'Exterior',
+                            expandButtonText: '+',
+                            evFeatures: [
+                                { 
+                                    name: 'Colour',
+                                    value: 'Red',
+                                },
+                                { 
+                                    name: 'Doors',
+                                    value: '5',
+                                },
+                            ],    
+                        },
+                        {
+                            name: 'Interior',
+                            expandButtonText: '+',
+                            evFeatures: [
+                                { 
+                                    name: 'Seats',
+                                    value: 'Leather',
+                                },
+                                { 
+                                    name: 'Screen',
+                                    value: 'Touch',
+                                },
+                            ],    
+                        },
+                        {
+                            name: 'Performance',
+                            expandButtonText: '+',
+                            evFeatures: [
+                                { 
+                                    name: '0-60mph',
+                                    value: '4.0sec',
+                                },
+                                { 
+                                    name: 'Top speed',
+                                    value: '162mph',
+                                },
+                            ],    
+                        },
+                    ],
+                },
+            }             
         }
         
         return (
             <div className="ev">
-                <h1>EV - {this.props.match.params.id}</h1>
                 <EVTitle title={ev.title} />
                 <EVPrice price={ev.price} />
                 <SellerContact {...ev.seller} />

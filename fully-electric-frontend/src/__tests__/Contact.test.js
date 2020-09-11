bb6c2b42e7f70dec41ee4db08907a1e4e5830e39
goncalog <goncalogordo@gmail.com>
2020-09-11 15:02:28 +0100

    Add tests for emailText and messageText properties that are passed from Contact React component

--- fully-electric-frontend/src/__tests__/Contact.test.js
+++ fully-electric-frontend/src/__tests__/Contact.test.js
@@ -37,5 +37,7 @@ describe('Contact', () => {
         const shallowWrapper = contact().find(ContactForm);
         expect(shallowWrapper.length).toEqual(1);
         expect(shallowWrapper.prop('legend')).toBe('Send your message:');
+        expect(Object.keys(shallowWrapper.props())).toContain('emailText');
+        expect(Object.keys(shallowWrapper.props())).toContain('messageText');
     });
 });

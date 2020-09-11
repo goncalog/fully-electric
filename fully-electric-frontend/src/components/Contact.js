f15549b2c583cada00805601a85e6008d9c6ea4d
goncalog <goncalogordo@gmail.com>
2020-09-11 15:12:53 +0100

    Add state to pass emailText and messageText properties from Contact React component

--- fully-electric-frontend/src/components/Contact.js
+++ fully-electric-frontend/src/components/Contact.js
@@ -3,10 +3,22 @@ import ContactForm from './support_components/ContactForm';
 import CallToActionButton from './support_components/CallToActionButton';
 
 export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailText: '',
+            messageText:'',
+        }
+    }
+
     render() {
         return (
             <div className="contact">
-                <ContactForm legend="Send your message:" />
+                <ContactForm 
+                    legend="Send your message:" 
+                    emailText={this.state.emailText} 
+                    messageText={this.state.messageText} 
+                />
                 <CallToActionButton callToActionText="Send Message"></CallToActionButton>
             </div>
         );

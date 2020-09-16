33d343fb023f8e17485b8cf5395970725fb2710d
goncalog <goncalogordo@gmail.com>
2020-09-16 14:08:10 +0100

    Add background image path property to SellerContainer React component

--- fully-electric-frontend/src/components/support_components/SellerContainer.js
+++ fully-electric-frontend/src/components/support_components/SellerContainer.js
@@ -4,7 +4,7 @@ import CallToActionButton from './CallToActionButton';
 
 export default function SellerContainer(props) {
     return (
-        <div className="seller-container">
+        <div className="seller-container" style={{backgroundImage: `url(${props.backgroundImagePath})` }}>
             <Headline 
                 mainHeadline={props.mainHeadline} 
                 secondaryHeadline={props.secondaryHeadline} 

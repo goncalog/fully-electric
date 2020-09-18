2141f43af794e145384218ce64aa106ec796718a
goncalog <goncalogordo@gmail.com>
2020-09-18 16:43:07 +0100

    Update formatPrice util function to use formatNumber util function

--- fully-electric-frontend/src/utils/formatPrice.js
+++ fully-electric-frontend/src/utils/formatPrice.js
@@ -1,11 +1,9 @@
+import formatNumber from './formatNumber';
+
 export default function formatPrice(country, price) {
     switch(country.toLowerCase()) {
         case 'uk':
-            if (parseInt(price) >= 1000) {
-                let array = price.split('');
-                return `£${array.splice(0, array.length - 3).join('')},${array.join('')}`;
-            }
-            return `£${price}`;
+            return `£${formatNumber(price)}`;
         default:
             return price;
     }

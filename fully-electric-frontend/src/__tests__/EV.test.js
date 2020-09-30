a419dbb921f4023d8038f767677ff2b6edd188d0
goncalog <goncalogordo@gmail.com>
2020-09-30 11:09:45 +0100

    Fix Error thrown by Jest because it doesn't implement the window.scrollTo function

--- fully-electric-frontend/src/__tests__/EV.test.js
+++ fully-electric-frontend/src/__tests__/EV.test.js
@@ -34,6 +34,18 @@ describe('EV', () => {
         shallowEV = undefined;
     });
 
+    // The default test environment for Jest is a browser-like environment provided by jsdom,
+    // which implements most of what an actual browser would provide, but it doesn't implement everything.
+    // Specifically, jsdom doesn't implement window.scrollTo, and instead throws an Error.
+    const jsdomScrollTo = window.scrollTo;  // remember the jsdom scrollTo
+    beforeAll(() => {
+        window.scrollTo = jest.fn(); // provide a mock implementation for window.scrollTo
+    });
+
+    afterAll(() => {
+        window.scrollTo = jsdomScrollTo; // restore the jsdom scrollTo
+    });
+
     test('has 5 children', () => {
         expect(ev().children().length).toEqual(5);
     });

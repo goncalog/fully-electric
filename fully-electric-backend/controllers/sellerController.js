fb1991044850bcb615cec5dd7e05739df620b770
goncalog <goncalogordo@gmail.com>
2020-10-05 21:57:01 +0100

    Send email with data received from contact seller POST request

--- fully-electric-backend/controllers/sellerController.js
+++ fully-electric-backend/controllers/sellerController.js
@@ -5,5 +5,42 @@ exports.getContactSeller = (req, res, next) => {
 
 // POST request to contact seller
 exports.postContactSeller = (req, res, next) => {
+    const nodemailer = require('nodemailer');
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // In case there's no real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // Create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass, // generated ethereal password
+            },
+        });
+
+        // Send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: req.body.from, // sender address
+            to: req.body.to, // list of receivers
+            subject: req.body.subject, // Subject line
+            text: req.body.text, // plain text body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+
+    main().catch(console.error);
+
     res.json({ title: `Contact seller with id ${req.params.id}` });
 }

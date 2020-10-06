8b7e67e618213b44e09df49be9036ee55d9bd4e4
goncalog <goncalogordo@gmail.com>
2020-10-06 21:44:12 +0100

    Add class to Input React components

--- fully-electric-frontend/src/components/support_components/Input.js
+++ fully-electric-frontend/src/components/support_components/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Input(props) {
     return (
-        <div>
+        <div className={props.className}>
             <input 
                 placeholder={props.placeholder} 
                 value={props.text}

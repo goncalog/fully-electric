a38827e8ffeeccdc3383410b57c5b55553bc5ae8
goncalog <goncalogordo@gmail.com>
2020-10-09 22:53:18 +0100

    Accept cors requests with cookies

--- fully-electric-backend/app.js
+++ fully-electric-backend/app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, '../fully-electric-frontend/build'))
 app.use(function (req, res, next) {
   // Website allowed to request content
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   next();

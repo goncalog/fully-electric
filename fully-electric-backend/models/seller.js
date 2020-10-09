30ea1df7e5b1d46a2a17a56ca734eb74601773db
goncalog <goncalogordo@gmail.com>
2020-10-09 17:22:59 +0100

    Update Seller contract property to be unique

--- fully-electric-backend/models/seller.js
+++ fully-electric-backend/models/seller.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const SellerSchema = new Schema({
     name: { type: String, required: true },
-    contact: { type: String, required: true },
+    contact: { type: String, required: true, unique: true },
     rating: { type: Number, required: true, min: 0, max: 5 },
     password: { type: String, required: true, minlength: 8 },
 });

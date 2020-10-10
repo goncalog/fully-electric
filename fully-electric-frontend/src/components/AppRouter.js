dfe7895ac731db6bdbd4f992243710bb1c7e250f
goncalog <goncalogordo@gmail.com>
2020-10-10 15:50:46 +0100

    Set state according to log in status

--- fully-electric-frontend/src/components/AppRouter.js
+++ fully-electric-frontend/src/components/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import EVs from './EVs';
@@ -9,9 +9,32 @@ import EV from './EV';
 import withAuth from './support_components/withAuth';
 
 function AppRouter() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        let url = (process.env.NODE_ENV === 'production')
+                ? '/content/seller/checkAuth' 
+                : `${process.env.REACT_APP_SERVER_URL}/content/seller/checkAuth`;
+
+        fetch(url, { credentials: 'include' })
+            .then(res => {
+                if (res.status === 200) {
+                    setLoggedIn(true);
+                } else {
+                    const error = new Error(res.error);
+                    throw error;
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                setLoggedIn(false);
+            });
+      });
+
     return (
         <Router>
-            <Navigation />
+            <Navigation loggedIn={loggedIn}/>
             <Switch>
                 <Route path='/' exact component={Home}></Route>
                 <Route path='/evs' exact component={EVs}></Route>

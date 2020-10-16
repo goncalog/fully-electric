6ba730cc2e89023e232a7bee287b404dddd2d003
goncalog <goncalogordo@gmail.com>
2020-10-16 22:39:05 +0100

    Add GET all makes route controller function

--- fully-electric-backend/controllers/makeController.js
+++ fully-electric-backend/controllers/makeController.js
@@ -1,4 +1,17 @@
+const Make = require('../models/make');
+
 // GET request for make
 exports.getMake = (req, res, next) => {
     res.json({ title: `Make with id ${req.params.id}` });
+}
+
+// GET request for list of all makes
+exports.getMakes = (req, res, next) => {
+    Make.find({})
+        .exec(function (err, makes) {
+            if (err) { return next(err); }
+
+            // Successful, so send data
+            res.json({ title: 'List of all makes', makes: makes });
+        });
 }

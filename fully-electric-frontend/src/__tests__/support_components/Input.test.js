c959c37eef80432046d3a542da607f86b2294983
goncalog <goncalogordo@gmail.com>
2020-10-16 15:54:17 +0100

    Remove tests for datalist HTML element from Input React component

--- fully-electric-frontend/src/__tests__/support_components/Input.test.js
+++ fully-electric-frontend/src/__tests__/support_components/Input.test.js
@@ -26,13 +26,12 @@ describe('Input', () => {
             placeholder: "Text to test placeholder property",
             text: "Text to test text property",
             onTextChange: mockFunction,
-            datalist: ['Text 1 to test datalist property', 'Text 2 to test datalist property'],
         }
         shallowInput = undefined;
     });
 
-    test('has 2 children with passed properties', () => {
-        expect(input().children().length).toEqual(2);
+    test('has one child with passed property', () => {
+        expect(input().children().length).toEqual(1);
         expect(input().hasClass(props.className)).toBe(true);
     });
 
@@ -42,22 +41,6 @@ describe('Input', () => {
         expect(shallowWrapper.prop('placeholder')).toBe(props.placeholder);
         expect(shallowWrapper.prop('value')).toBe(props.text);
         expect(Object.keys(shallowWrapper.props())).toContain('type');
-        expect(shallowWrapper.prop('list')).toBe(props.property);
-    });
-
-    test('has one HTML datalist element', () => {
-        const shallowWrapper = input().find('datalist');
-        expect(shallowWrapper.length).toEqual(1);
-        expect(shallowWrapper.prop('id')).toBe(props.property);
-    });
-
-    test('has 2 HTML option elements', () => {
-        const shallowWrapper = input().find('option');
-        expect(shallowWrapper.length).toEqual(2);
-        shallowWrapper.forEach((node, index) => {
-            expect(node.key()).toBe(index.toString());
-            expect(node.prop('value')).toBe(props.datalist[index]);
-        });
     });
 
     test('should call mockFunction onTextChange', () => {

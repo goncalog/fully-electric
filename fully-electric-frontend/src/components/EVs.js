57646eac2a409512ec8ccbd5078536411e4f5f05
goncalog <goncalogordo@gmail.com>
2020-11-10 14:46:39 +0000

    Add DropDowns to the EVs React component

--- fully-electric-frontend/src/components/EVs.js
+++ fully-electric-frontend/src/components/EVs.js
@@ -4,11 +4,22 @@ import getFullEvTitle from '../utils/getFullEvTitle';
 import '../css/EVs.css';
 import formatMiles from '../utils/formatMiles';
 import formatNumber from '../utils/formatNumber';
+import DropDown from './support_components/DropDown';
 
 export default class EVs extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { evs: [] };
+        this.state = { 
+            evs: [], 
+            makes: [{ name: 'Tesla' }, { name: 'Nissan' }, { name: 'Renault' }],
+            models: [{ name: 'X' }, { name: 'S' }, { name: '3' }],
+            filterVisibility: { make: false, model: false }, 
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(property) {
+        this.setState({ filterVisibility: { [property]: !this.state.filterVisibility[property] }});
     }
 
     componentDidMount() {
@@ -48,6 +59,24 @@ export default class EVs extends React.Component {
 
         return (
             <div className="evs">
+                <div className="filters">
+                    <DropDown 
+                        property="make"
+                        title="Make"
+                        onClick={this.handleClick}
+                        // option={this.state.make} 
+                        options={this.state.makes}
+                        visibility={this.state.filterVisibility.make}
+                    />
+                    <DropDown 
+                        property="model"
+                        title="Model"
+                        onClick={this.handleClick}
+                        // option={this.state.model} 
+                        options={this.state.models}
+                        visibility={this.state.filterVisibility.model}
+                    />
+                </div>
                 <EVsContainer evs={evs} {...this.props} />
             </div>
         );

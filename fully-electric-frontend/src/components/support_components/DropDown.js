87b63c530025f605238d3b755445a3597956f6b5
goncalog <goncalogordo@gmail.com>
2020-11-10 17:24:53 +0000

    Update DropDown to include MinMax type

--- fully-electric-frontend/src/components/support_components/DropDown.js
+++ fully-electric-frontend/src/components/support_components/DropDown.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import MinMax from '../support_components/MinMax';
 
 export default function DropDown(props) {
     function handleButtonClick() {
         props.onClick(props.property);
     }
 
+    function handleTextChange(type, text) {
+        props.onTextChange(props.property, type, text);
+    }
+
     return (
         <div className={`dropdown ${props.property}`}>
             <button className="dropdown-button" onClick={handleButtonClick}>{`${props.title} ▾`}</button>
             <div className={props.visibility ? "dropdown-content show" : "dropdown-content"}>
-                {props.options.map((item, key) => {
-                    return (
-                        <div key={key}>                            
-                            <label for={key}>
-                                <input type="checkbox" key={key} id={key} value={item._id}></input>
-                                {`${item.name ? item.name : item.city}`}
-                            </label>
-                        </div>
-                    );
-                })}
+                {props.type === 'minMax' 
+                    ? (
+                        <MinMax
+                            min={props.min}
+                            max={props.max}
+                            onTextChange={handleTextChange}
+                        />
+                    )
+                    : props.options.map((item, key) => {
+                        return (
+                            <div key={key}>                            
+                                <label for={key}>
+                                    <input type="checkbox" key={key} id={key} value={item._id}></input>
+                                    {`${item.name ? item.name : item.city}`}
+                                </label>
+                            </div>
+                        );
+                    })}
             </div>
         </div>
     );

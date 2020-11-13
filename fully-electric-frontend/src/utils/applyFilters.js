d4e4c5f41ed48333d42aff0d22bb6778d3079d18
goncalog <goncalogordo@gmail.com>
2020-11-13 22:13:11 +0000

    Add remainder filters (price, mileage, range, extras)

--- fully-electric-frontend/src/utils/applyFilters.js
+++ fully-electric-frontend/src/utils/applyFilters.js
@@ -3,5 +3,35 @@ export default function applyFilters(state) {
     if (filteredMakes.length === 0) {
         filteredMakes = state.make.options.slice().map((make) => make._id);
     }
-    return state.evs.slice().filter((ev) => filteredMakes.includes(ev.make._id));
+
+    let filteredEvs = state.evs.slice().filter((ev) => filteredMakes.includes(ev.make._id));
+
+    if (state.price.min !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.price >= parseInt(state.price.min)));
+    }
+    if (state.price.max !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.price <= parseInt(state.price.max)));
+    }
+
+    if (state.mileage.min !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.mileage >= parseInt(state.mileage.min)));
+    }
+    if (state.mileage.max !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.mileage <= parseInt(state.mileage.max)));
+    }
+
+    if (state.range.min !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.model.charging.range_miles >= parseInt(state.range.min)));
+    }
+    if (state.range.max !== "") {
+        filteredEvs = filteredEvs.filter((ev) => (ev.model.charging.range_miles <= parseInt(state.range.max)));
+    }
+
+    state.extras.options.forEach((option) => {
+        if (option.checked) {
+            filteredEvs = filteredEvs.filter((ev) => ev.equipment_and_options.includes(option.name));
+        }
+    });
+
+    return filteredEvs;
 }

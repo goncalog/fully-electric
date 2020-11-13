5ac6590272d9a743b59950a45b072d49bb58262d
goncalog <goncalogordo@gmail.com>
2020-11-13 15:07:23 +0000

    Update CheckBox to include onChange prop

--- fully-electric-frontend/src/components/support_components/CheckBox.js
+++ fully-electric-frontend/src/components/support_components/CheckBox.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 export default function CheckBox(props) {
+    function handleChange(e) {
+        props.onChange(e.target.id);
+    }
+
     return (
         <div className="checkbox">
             {props.options.map((item, key) => {
                 return (
                     <div key={key}>                            
                         <label for={key}>
-                            <input type="checkbox" key={key} id={key} value={item._id}></input>
+                            <input 
+                                type="checkbox" 
+                                key={key} 
+                                id={key} 
+                                value={item._id} 
+                                checked={item.checked}
+                                onChange={handleChange}
+                            >
+                            </input>
                             {`${item.name ? item.name : item.city}`}
                         </label>
                     </div>
